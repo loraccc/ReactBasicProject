1fd79cb4fdb380cfdac18aacca113aa1f7ff3d00
Carol Acharya <carolacharya@Carols-MacBook-Air.local>
2023-09-19 11:14:35 +0545

    Pages with css& Footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 import Menu from './pages/Menu';
 import About from './pages/About';
@@ -18,12 +19,12 @@ function App() {
 <Router>
   <Navbar/>
   <Routes>
-  <Route path='/' element={Home}/>
+  <Route path='/' element={<Home/>}/>
   <Route path='/menu' element={<Menu/>}/>
   <Route path='/about' element={<About/>}/>
   <Route path='/contact' element={<Contact/>}/>
 </Routes>
-
+<Footer/>
   </Router>
       
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
+import Heart from '../assets/heart.png'
+import '../styles/Home.css'
 
 function Home() {
   return (
     <div>
-      <h1>HEllo WOELD</h1>
+      <div className='home'>
+        <div className='headerContainer' 
+        style={{backgroundImage : `url(${Heart})`}}
+        >
+          <h1>Carol's Cars</h1>
+          <p>Cars for any Personality !!!</p>
+          
+            <Link to='/menu'>
+            <button>
+              Order Now
+              </button>
+              </Link>
+          
+
+
+        </div>
+
+
+      </div>
     </div>
   )
 }
 
-export default Home
+export default Home;
