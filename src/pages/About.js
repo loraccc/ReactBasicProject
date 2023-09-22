80cfc56e61044d182d5f4b9c4b65f4425dc3c765
Carol Acharya <carolacharya@Carols-MacBook-Air.local>
2023-09-22 12:35:54 +0545

    About us page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
+import carset from "../assets/carset.webp";
+import "../styles/About.css";
 
 function about() {
   return (
-    <div>
-      <h1>HELLO IM CARS</h1>
+    <div className="about">
+      <div
+        className="aboutTop"
+        style={{ backgroundImage: `url(${carset})` }}
+      ></div>
+      <div className="aboutBottom">
+        <h1>About Us</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
+          corrupti vero neque, facilis soluta, totam voluptatem vel vitae optio
+          nam ratione iusto aperiam dolore itaque ex voluptate voluptas impedit
+          mollitia. Harum voluptate alias reprehenderit dolor consectetur
+          nesciunt blanditiis, repellat hic pariatur dolorem tempore id delectus
+          nostrum dignissimos obcaecati consequuntur fugit ipsam quas voluptatum
+          recusandae quos esse tempora corporis! Corporis labore excepturi
+          quidem totam, provident natus at iure rem nisi nihil voluptatum
+          quaerat harum illum quae veniam dicta perspiciatis consequuntur
+          reprehenderit tempora suscipit quas quisquam. Error quasi accusantium
+          nobis dolorum perferendis deserunt vero sed, ducimus tempore. Pariatur
+          iste hic molestias itaque saepe fugit expedita voluptatum, ipsum
+          dolorum eaque? Dicta ab voluptate perspiciatis quidem consequuntur ad
+          similique quaerat ratione. Quibusdam, iusto nisi? Cupiditate vero
+          perferendis quibusdam odit libero suscipit assumenda commodi modi
+          quisquam porro tenetur rem at ratione saepe esse, debitis minima
+          voluptatibus facere obcaecati! Debitis iste quaerat, quis placeat non
+          eaque fugit dolorem perferendis velit odit corporis quae facere? Nam
+          cum qui vero. Tenetur nemo ullam, quas consectetur doloribus quae
+          expedita harum voluptate nobis, veritatis unde dicta ipsum dignissimos
+          repellendus aliquid labore placeat. Ipsum similique, sed reiciendis
+          explicabo nisi ducimus ratione. Voluptatem illum vitae cupiditate
+          totam saepe expedita. Temporibus, incidunt error.
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default about
+export default about;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div>
-      <h1>CONTACT US </h1>
+    <div className='contactLeftside'>
+      
     </div>
   )
 }
